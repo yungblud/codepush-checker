15c27f535ddd8900a0d61910c44036982da2ee37
yungblud <killi8n@gmail.com>
2022-03-07 21:35:57 +0900

    init

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import codePush, { LocalPackage } from 'react-native-code-push'
+import { AppState } from 'react-native'
+import { useEffect } from 'react'
+
+export function useCodePushCheck({ onSync, onSyncError, onCheckFinished }: { onSync?: (meta: LocalPackage | null) => void | Promise<void>, onSyncError?: (e: Error) => void | Promise<void>, onCheckFinished?: () => void | Promise<void> }) {
+    useEffect(() => {
+        const listener = async () => {
+            try {
+                const availableUpdate = await codePush.checkForUpdate();
+                if (availableUpdate) {
+                    await codePush.sync({
+                        installMode: codePush.InstallMode.IMMEDIATE,
+                    });
+                    const meta = await codePush.getUpdateMetadata()
+                    onSync && onSync(meta)
+                }
+            } catch (e) {
+                onSyncError && onSyncError(e as Error)
+            } finally {
+                onCheckFinished && onCheckFinished()
+            }
+        }
+        const sub = AppState.addEventListener("change", listener)
+        return () => {
+            sub.remove()
+        }
+    }, [])
+}
